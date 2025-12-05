c7b7f002f0a57b944d70cf81eb78a05390c87b66
Hikma1 <hikma0227@gmail.com>
2025-12-05 23:15:35 +0300

    new js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const passwordInput = document.getElementById("passwordInput");
 const strengthBar = document.getElementById("strengthBar");
 const feedback = document.getElementById("feedback");
+passwordInput.addEventListener("input", analyzePassword);
+
+function analyzePassword() {
+  const password = passwordInput.value;
+  let score = 0;
