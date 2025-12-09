5bab50df9d1818b90852505fc1e73bc23bcbcbb8
Hikma1 <hikma0227@gmail.com>
2025-12-09 15:56:03 +0300

    app.js

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 const passwordInput = document.getElementById("passwordInput");
 const strengthBar = document.getElementById("strengthBar");
 const feedback = document.getElementById("feedback");
+// Requirement elements
+const reqLength = document.getElementById("req-length");
+const reqUpper = document.getElementById("req-upper");
+const reqLower = document.getElementById("req-lower");
+const reqNumber = document.getElementById("req-number");
+const reqSymbol = document.getElementById("req-symbol");
