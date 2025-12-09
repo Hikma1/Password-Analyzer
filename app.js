15cc234302d3bd352c01c24487d18752fad8df17
Hikma1 <hikma0227@gmail.com>
2025-12-09 15:55:12 +0300

    app.js

--- app.js
+++ app.js
@@ -1,44 +1,4 @@
+// Select elements
 const passwordInput = document.getElementById("passwordInput");
 const strengthBar = document.getElementById("strengthBar");
 const feedback = document.getElementById("feedback");
-passwordInput.addEventListener("input", analyzePassword);
-
-function analyzePassword() {
-  const password = passwordInput.value;
-  let score = 0;
-   if (password.length >= 8) score += 20;
-  if (/[0-9]/.test(password)) score += 20;
-  if (/[A-Z]/.test(password)) score += 20;
-  if (/[a-z]/.test(password)) score += 20;
-  if (/[^A-Za-z0-9]/.test(password)) score += 20;
-
-  let color = "red";
-  let text = "Very Weak";
-
-  if (score <= 20) {
-    color = "red";
-    text = "Very Weak";
-  } 
-  else if (score <= 40) {
-    color = "orange";
-    text = "Weak";
-  }
-  else if (score <= 60) {
-    color = "goldenrod";
-    text = "Medium";
-  }
-  else if (score <= 80) {
-    color = "green";
-    text = "Strong";
-  }
-  else {
-    color = "darkgreen";
-    text = "Very Strong";
-  }
-   // Visual update
-  strengthBar.style.width = score + "%";
-  strengthBar.style.backgroundColor = color;
-
-  feedback.textContent = text;
-  feedback.style.color = color;
-}
