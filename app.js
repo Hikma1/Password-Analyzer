08b8ad328cd0026be15e07c5b152c45a60104164
Hikma1 <hikma0227@gmail.com>
2026-01-05 17:34:16 +0300

    js

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 const passwordInput = document.getElementById("passwordInput");
 const progressBar = document.getElementById("progressBar");
+const feedback = document.getElementById("feedback");
+const toggleEye = document.getElementById("toggleEye");
+const themeToggle = document.getElementById("themeToggle");
