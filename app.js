7aa4face31d65beeb15e76f691cce4d7bdc7d2f6
Hikma1 <hikma0227@gmail.com>
2026-02-04 12:26:32 +0300

    another

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ const passwordInput = document.getElementById("passwordInput");
 const strengthBar = document.getElementById("strengthBar");
 const feedback = document.getElementById("feedback");
 const toggleEye = document.getElementById("toggleEye");
+/* Eye Toggle */
+toggleEye.addEventListener("click", () => {
+});
