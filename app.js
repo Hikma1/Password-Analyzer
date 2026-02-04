2e1cdb5944a9b4a285529bc7f0ad5c9cb24c5572
Hikma1 <hikma0227@gmail.com>
2026-02-04 12:18:48 +0300

    add

---
+++ app.js
@@ -0,0 +1,4 @@
+const passwordInput = document.getElementById("passwordInput");
+const strengthBar = document.getElementById("strengthBar");
+const feedback = document.getElementById("feedback");
+const toggleEye = document.getElementById("toggleEye");
